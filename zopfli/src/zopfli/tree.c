5d750877f020c45421b843cee2417514cf6aeb6f
Megan Andersen <meganandersen@Megans-MBP-8.attlocal.net>
2020-04-29 20:40:36 -0700

    adding back changes

--- zopfli/src/zopfli/tree.c
+++ zopfli/src/zopfli/tree.c
@@ -1,18 +1,14 @@
 /*
 Copyright 2011 Google Inc. All Rights Reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
 Author: lode.vandevenne@gmail.com (Lode Vandevenne)
 Author: jyrki.alakuijala@gmail.com (Jyrki Alakuijala)
 */
@@ -38,15 +34,15 @@ pthread_mutex_t lock;
 
 struct args {
     unsigned* symbols;
-	size_t n;
+  size_t n;
 };
 
 void *initializeSymbols(void *daArgs) { 
-	pthread_mutex_lock(&lock);
-	for (int i = 0; i < ((struct args*)daArgs)->n; i++) {
-	    ((struct args*)daArgs)->symbols[i] = 0;
-	}
-	pthread_mutex_unlock(&lock);
+  pthread_mutex_lock(&lock);
+  for (int i = 0; i < ((struct args*)daArgs)->n; i++) {
+      ((struct args*)daArgs)->symbols[i] = 0;
+  }
+  pthread_mutex_unlock(&lock);
 } 
 
 void ZopfliLengthsToSymbols(const unsigned* lengths, size_t n, unsigned maxbits,
@@ -56,7 +52,7 @@ void ZopfliLengthsToSymbols(const unsigned* lengths, size_t n, unsigned maxbits,
   unsigned bits, i;
   unsigned code;
 
- 	struct args *daArgs;
+  struct args *daArgs;
     daArgs->n = n;
     daArgs->symbols = &symbols;
     pthread_t thread1; 
@@ -108,7 +104,7 @@ void ZopfliLengthsToSymbols(const unsigned* lengths, size_t n, unsigned maxbits,
 void ZopfliCalculateEntropy(const size_t* count, size_t n, double* bitlengths) {
   static const double kInvLog2 = 1.4426950408889;  /* 1.0 / log(2.0) */
 
-	pthread_t thread_id;
+  pthread_t thread_id;
 
 
   unsigned sum = 0;
@@ -141,5 +137,3 @@ void ZopfliCalculateBitLengths(const size_t* count, size_t n, int maxbits,
   (void) error;
   assert(!error);
 }
-
-
